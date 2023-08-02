c1fc996d9c9f22a7a3d666edf66ace33d626d8ba
pirmanabd <pirman.abdurohman@moladin.com>
2023-08-02 13:04:36 +0700

    define migration & models

--- home/enumerate.py
+++ home/enumerate.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 class ProjectType(Enum):
-  MFI = "MFI"
-  MDI = "MDI"
+  MFI = "Moladin Finance Indonesia"
+  MDI = "Moladin Digital Indonesia"
   
   @classmethod
   def choices(cls):
@@ -12,6 +12,14 @@ class EnvironmentType(Enum):
   DEVL = "development"
   STAG = "staging"
   PROD = "production"
+
+  @classmethod
+  def choices(cls):
+    return [(key.value, key.name) for key in cls]
+
+class ServiceType(Enum):
+  BE = "backend"
+  FE = "frontend"
 
   @classmethod
   def choices(cls):
--- home/models.py
+++ home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from .enumerate import ProjectType, EnvironmentType
+from .enumerate import ProjectType, EnvironmentType, ServiceType
 # Create your models here.
 
 class TechFamily(models.Model):
@@ -11,7 +11,7 @@ class TechFamily(models.Model):
   pic_email = models.EmailField(max_length=25)
   slug = models.CharField(max_length=25)
   project = models.CharField(
-      max_length=5,
+      max_length=30,
       choices=ProjectType.choices()
     )
   created_at = models.DateTimeField(auto_now_add = True, auto_now = False, blank = False)
@@ -37,3 +37,40 @@ class IndexWeight(models.Model):
 
     def __str__(self):
         return self.value
+
+class Service(models.Model):
+    class Meta:
+        db_table = "service"
+
+    name = models.CharField(max_length = 100)
+    service_type = models.CharField(max_length=10, choices=ServiceType.choices())
+    project = models.CharField(max_length=30, choices=ProjectType.choices())
+    
+    tech_family = models.ForeignKey(
+        TechFamily,
+        on_delete=models.PROTECT,
+        blank=False
+    )
+
+    created_at = models.DateTimeField(auto_now_add = True, auto_now = False, blank = False)
+    updated_at = models.DateTimeField(auto_now = False, blank = False)
+
+    def __str__(self):
+        return self.name
+
+class Metric(models.Model):
+  service = models.ForeignKey(
+        Service,
+        on_delete=models.PROTECT,
+        blank=False
+    )
+
+  environment = models.CharField(max_length=12, choices=EnvironmentType.choices())
+  date = models.DateField(blank=False)
+  cpu_usage = models.FloatField()
+  memory_usage = models.FloatField()
+  total_cost = models.FloatField()
+
+  created_at = models.DateTimeField(auto_now_add = True, auto_now = False, blank = False)
+  updated_at = models.DateTimeField(auto_now = False, blank = False)
+  
