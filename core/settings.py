c785aeedd1bac133b8fb403abcd03a4ea3a49f7e
Maulana Ibrahim <maulana.ibrahim@moladin.com>
2023-08-03 16:22:54 +0800

    update ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -31,10 +31,10 @@ if not SECRET_KEY:
 DEBUG = 'RENDER' not in os.environ
 
 # Docker HOST
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1', 'cost-management.moladin.com']
 
 # Add here your deployment HOSTS
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000/', 'http://0.0.0.0:8000/']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000/', 'http://0.0.0.0:8000/', 'http://cost-management.moladin.com/','https://cost-management.moladin.com/']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
