744b945d6d791c9c96d7e36a00c4532fb1e70883
pirmanabd <pirman.abdurohman@moladin.com>
2023-08-07 11:02:25 +0700

    implement async

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 
 from .views.bigquery_views import BigQueryViews
-from .views.report_views import index, async_view, sync_view, test_async
+from .views.report_views import test_async
 
 urlpatterns = [
     path('get-project', BigQueryViews.as_view()),
     path('get-tf', BigQueryViews.get_tf),
     path('sync-index', BigQueryViews.post_index_weight),
     path("test", test_async),
-    path("", index)
 ]
